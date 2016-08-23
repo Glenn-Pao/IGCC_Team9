954b3a8569d3369d0828577db7aea1b2dfef6bef
Glenn-Pao <140522J@mymailnyp.edu.sg>
2016-08-23 15:55:08 +0800

    Sample microphone input is now set.

---
+++ TeleKinesisVR/Assets/Scripts/Classes/Microphone_Input.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(AudioSource))]
+public class Microphone_Input : MonoBehaviour
+{
+    AudioSource src;
+
+    void Start()
+    {
+        src = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.F1))
+        {
+            src.clip = Microphone.Start(null, true, 10, 44100);
+        }
+        if (Input.GetKeyDown(KeyCode.F2))
+        {
+            Microphone.End(null);
+            src.Play(); // Play the audio source!
+        }
+    }
+}
