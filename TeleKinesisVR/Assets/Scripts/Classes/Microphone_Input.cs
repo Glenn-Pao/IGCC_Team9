e4b9560bcc1f840b160e1738e3bf5e90e3f7d188
Glenn-Pao <140522J@mymail.nyp.edu.sg>
2016-08-23 20:03:22 +0800

    Mic threshold detection is now working.

--- TeleKinesisVR/Assets/Scripts/Classes/Microphone_Input.cs
+++ TeleKinesisVR/Assets/Scripts/Classes/Microphone_Input.cs
@@ -4,24 +4,92 @@ using System.Collections;
 [RequireComponent(typeof(AudioSource))]
 public class Microphone_Input : MonoBehaviour
 {
-    AudioSource src;
+    private static int frequency = 44100;   //frequency of sound
+    private static int sampleCount = 256;   //time samples
+
+    private float[] samples;                //the array of samples
+
+    AudioSource src;                     //the audio source of the microphone
+    float threshold = 1.5f;              //threshold required to exceed in order to begin voice recognition
+    float loudness = 0.0f;               //loudness of microphone input
+    public float sensitivity = 100.0f;   //the sensitivity of the microphone
+
+    public bool volumeLoud = false;      //debugging for loudness check
 
     void Start()
     {
+        //get the component of audio source
         src = GetComponent<AudioSource>();
+        samples = new float[sampleCount];   //an array of 256 sample points
+
+        //Begin the microphone listening
+        StartMicListener();
+    }
+    
+    //starts the microphone recording
+    private void StartMicListener()
+    {
+        src.clip = Microphone.Start(null, true, 10, frequency);
+
+        //waits for microphone input
+        while(!(Microphone.GetPosition(null) > 0))
+        {
+            src.Play();
+            //src.mute = true;    //Mute yourself
+        }
     }
 
     void Update()
     {
-
-        if (Input.GetKeyDown(KeyCode.F1))
+        //restart the audio if it stopped playing
+        if(!src.isPlaying)
         {
-            src.clip = Microphone.Start(null, true, 10, 44100);
+            StartMicListener();
         }
+
+        //Determine if voice is loud enough
+        voiceLoudEnough();
+
         if (Input.GetKeyDown(KeyCode.F2))
         {
             Microphone.End(null);
             src.Play(); // Play the audio source!
         }
+        //check the loudness here
+        voiceLoudEnough();
+    }
+
+    //analyze the sound samples
+    float GetAverageVolume()
+    {
+        //the summation of numbers
+        float sum = 0.0f;
+
+        //get the block of output of sound data
+        src.GetOutputData(samples, 0);
+
+        for (int i = 0; i < sampleCount; i++)
+        {
+            sum += Mathf.Abs(samples[i]);
+        }
+
+        return sum / sampleCount;
+    }
+
+    //determine the loudness of the sound
+    //return true if it exceeds the threshold
+    bool voiceLoudEnough()
+    {
+        //determine the loudness of the sound
+        loudness = GetAverageVolume() * sensitivity;
+
+        //if it exceeds the threshold
+        if(loudness > threshold)
+        {
+            Debug.Log("Loudness: " + loudness);
+            return true;
+        }
+        //if it doesn't, then return false
+        return false;
     }
 }
