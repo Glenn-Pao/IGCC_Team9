261738bbf9ccdfa19debbb987cab67dd32447516
unknown <juusom@M314G404.sidm.student.local>
2016-08-24 15:49:23 +0800

    WeaponBranch test

---
+++ TeleKinesisVR/Assets/Scripts/Classes/UnitBehavior.cs
@@ -0,0 +1,173 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnitBehavior : MonoBehaviour {
+
+ public   Stance Soldier = Stance.IDLE;
+    AttackType SoldierAttack = AttackType.IDLE;
+   public int Types = 0;
+
+
+    public enum Stance
+    {
+        COMBAT,
+        MOVE,
+        RETREAT,
+        FLEE,
+        IDLE
+    };
+    public enum AttackType
+    {
+        MELEE,
+        RANGED,
+        IDLE
+    };
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        switch(Soldier)
+        {  
+            case Stance.IDLE:
+        {
+            break;
+        }
+
+            case Stance.COMBAT:
+        {
+            if (SoldierAttack == AttackType.MELEE)
+            { 
+                // Soldiers do Melee attack
+                Soldier = Stance.IDLE;
+                break;
+                 
+            }
+
+            else if (SoldierAttack == AttackType.RANGED)
+            {
+                //Soldier do Ranged attack
+                Soldier = Stance.IDLE;
+                break;
+            }
+            else
+            {
+                Soldier = Stance.IDLE;
+                break;
+            }
+            
+        }
+            case Stance.MOVE:
+        {
+
+            //if(waypoint == current position)
+            Soldier = Stance.IDLE; 
+
+            break;
+        }
+            case Stance.RETREAT:
+        {
+
+            //if(current position == waypoint[i-1])
+            Soldier = Stance.IDLE;
+
+            break;
+        }
+            case Stance.FLEE:
+        {
+            //if(current position == fleePosition)
+            //Delete unit
+
+            break;
+        }
+
+    }
+      
+
+	
+	}
+       
+    public void setStance(Stance Soldier)
+    {
+        this.Soldier = Soldier;
+    }
+    Stance getStance()
+    {
+        return Soldier;
+    }
+
+
+    public void setAttackType(AttackType SoldierAttack)
+    {
+        this.SoldierAttack = SoldierAttack;
+    }
+    AttackType GetAttackType()
+    { 
+        return SoldierAttack;
+    }
+
+  public  Stance SetStance(int Type)
+    {
+           switch(Type)
+           {
+               case 0:
+                   { Soldier = Stance.COMBAT;
+                   return Soldier;
+                   }
+               case 1:
+                   { Soldier = Stance.MOVE;
+                   return Soldier;
+                   }
+               case 2:
+                   {
+                       Soldier = Stance.RETREAT;
+                   return Soldier;
+                   }
+               case 3:
+                   {
+                       Soldier = Stance.FLEE;
+                   return Soldier;
+                   }
+
+               default:
+                   {
+                       Soldier = Stance.IDLE;
+                       return Soldier;
+                   }
+                   
+        
+                 
+                  
+           }
+
+    }
+    public AttackType SetAttackType(int Type)
+    {
+        switch(Type)
+        {
+            case 0:
+                {
+                    SoldierAttack = AttackType.MELEE;
+                    return SoldierAttack;
+                }
+            case 1:
+                {
+                    SoldierAttack = AttackType.RANGED;
+                    return SoldierAttack;
+
+                }
+            default:
+                {
+                    SoldierAttack = AttackType.IDLE;
+                    return SoldierAttack;
+                }
+        }
+
+    }
+}
---
+++ TeleKinesisVR/Assets/Scripts/Classes/UnitClass.cs
@@ -0,0 +1,110 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnitClass : MonoBehaviour {
+    UnitBehavior behavior;
+
+    public int Ammunition;
+    public int Health;
+    bool Morale = true;
+    int Difficulty = 4;
+    int Value;
+    public int UnitType;
+    Unit Units = Unit.PIKEMAN; 
+
+    enum Unit
+    {
+        PIKEMAN,
+        ARCHER,
+        BALLISTA,
+        MONSTER
+    }
+
+
+	// Use this for initialization
+	void Start () {
+        SetDifficulty( Value);
+  
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+if(Difficulty == 4 && Health <= 75 ){ Morale = false; }
+
+
+else if (Difficulty == 3 && Health <= 60) { Morale = false; }
+      
+
+else if (Difficulty == 2 && Health <= 45){ Morale = false; }
+    
+
+else if (Difficulty == 1 && Health <= 25){ Morale = false; }
+   
+
+
+        if (Morale == false)
+        {
+            behavior.setStance(behavior.Soldier);
+            return;
+        }
+
+	
+	}
+
+   void SetDifficulty(int value)
+    {
+        Difficulty = value;
+
+
+    }
+
+
+   public int ReduceHealth(int Reduce)
+    {
+        Health -=Reduce;
+        return Health;
+    }
+
+    public int ReduceAmmunition(int Ammunit)
+   {
+       Ammunition -= Ammunit;
+       return Ammunition;
+   }
+
+    public void SetUnit()
+    {
+
+    }
+
+    public void setUnitType(Unit unit)
+    {
+   this.Units = unit; 
+    }
+    public Unit SetUnitType(int Type)
+    {
+        switch (Type)
+        {
+            case 0:
+                {
+                    return Unit.PIKEMAN;
+                }
+            case 1:
+                {
+                    return Unit.ARCHER;
+                }
+            case 2:
+                {
+                    return Unit.BALLISTA;
+                }
+            case 3:
+                {
+                    return Unit.MONSTER;
+                }
+            default:
+                { return 0; }
+        }
+    }
+}
